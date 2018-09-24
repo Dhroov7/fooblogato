ec9f4088fc8468c507bd0f125787a9cd3a190e24
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-09-25 00:58:49 +0530

    blog pages route added

--- src/routes/pages/blog.js
+++ src/routes/pages/blog.js
@@ -3,4 +3,46 @@
  */
 
 const route = require('express').Router()
+const blogController = require('../../controllers/blog')
+const userController = require('../../controllers/user')
 
+route.get('/me', (req, res) => {
+
+    const blogs = blogController.findAllBlogs(req.user.id)
+
+    if (!blogs.length) {
+        res.status(404).send('No blogs found!')
+    }
+
+    res.render('homePage', {blogs})
+})
+
+route.get('/:username', (req, res) => {
+
+    const user = userController.findUser(req.params.username)
+
+    if (!user) {
+        res.status(404).send('No user found!')
+    }
+
+    const blogs = blogController.findAllBlogs(user.id)
+
+    if (!blogs.length) {
+        res.status(404).send('No blogs found!')
+    }
+
+    res.render('homePage', {blogs})
+})
+
+route.get('/:id', (req, res) => {
+
+    const blog = blogController.findBlog(req.params.id)
+
+    if (!blog) {
+        res.status(404).send('No blog found!')
+    }
+
+    res.render('feed',{blog})
+})
+
+module.exports = route
