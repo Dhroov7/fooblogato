db49848d415f4abbb90fa8612f5333c6a8486d61
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-09-24 13:39:16 +0530

    some changes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-
 /**
  * Created by dhroovgupta7 on 06/09/18
  */
@@ -16,20 +15,17 @@ const apiRouter = require('./routes/api')
 // const pageRouter = require('./routes/pages')
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended: true}))
 app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(session({
     secret: 'some random secret',
-    resave: true,
+    resave: false,
     saveUninitialized: true,
-    name: 'fooblogato',
-    cookie: {
-        domain: config.COOKIE_DOMAIN,
-        maxAge: 86400000
+    cookie:{
+        secure: true
     }
 }))
-app.use(express.session({ secret: 'some secret shit' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -41,14 +37,14 @@ app.engine('hbs', exphbs.express4({
 }))
 
 
-app.set('views',path.join(__dirname + '../views'))
-app.set('view engine','hbs')
-app.set('view cache',true)
+app.set('views', path.join(__dirname + '../views'))
+app.set('view engine', 'hbs')
+app.set('view cache', true)
 app.use('/', express.static(path.join(__dirname, '../public')))
 
 app.use('/api', apiRouter)
 // app.use('/', pageRouter)
 
-app.listen(9898,() => { //change this when deploy this in prod.
+app.listen(9898, () => { //change this when deploy this in prod.
     console.log('server started at http://localhost:9898')
 })
