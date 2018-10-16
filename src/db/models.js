42178a7a094b7964fd0aab74a52dc5a1b2a449db
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-10-16 21:14:12 +0530

    added default value of treats

--- src/db/models.js
+++ src/db/models.js
@@ -37,7 +37,7 @@ const blog = db.define('blog', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     title: {type: DataTypes.STRING, allowNull: false},
     description: {type: DataTypes.TEXT, allowNull: false},
-    treat: {type: DataTypes.INTEGER, allowNull: false}
+    treat: {type: DataTypes.INTEGER, allowNull: false, defaultValue: 0}
 })
 
 blog.belongsTo(user)
